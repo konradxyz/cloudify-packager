f7461d73e21df60cdd779cda5b492a6860a63cde
limor-gs <limor@gigaspaces.com>
2015-01-01 11:39:08 +0200

    correct version

--- docker/vars.py
+++ docker/vars.py
@@ -105,9 +105,9 @@ VARS = {
             "pyzmq==14.3.1"
         ],
         "modules": {
-                "cloudify_rest_client": "git+git://github.com/cloudify-cosmo/cloudify-rest-client.git@3.2a1",
-                "cloudify_plugins_common": "git+git://github.com/cloudify-cosmo/cloudify-plugins-common.git@3.2a1",
-                "cloudify_script_plugin": "git+git://github.com/cloudify-cosmo/cloudify-script-plugin.git@1.2a1",
+                "cloudify_rest_client": "git+git://github.com/cloudify-cosmo/cloudify-rest-client.git@3.2m1",
+                "cloudify_plugins_common": "git+git://github.com/cloudify-cosmo/cloudify-plugins-common.git@3.2m1",
+                "cloudify_script_plugin": "git+git://github.com/cloudify-cosmo/cloudify-script-plugin.git@1.2m1",
                 "cloudify_manager": "-b 3.2m1 https://github.com/cloudify-cosmo/cloudify-manager.git",
             },
         "workers_autoscale": "5,2",
@@ -120,8 +120,8 @@ VARS = {
             "python2.7"
         ],
         "modules": {
-            "cloudify_amqp_influxdb": "git+git://github.com/cloudify-cosmo/cloudify-amqp-influxdb.git@3.2a1",
-            "cloudify_dsl_parser": "git+git://github.com/cloudify-cosmo/cloudify-dsl-parser.git@3.2a1",
+            "cloudify_amqp_influxdb": "git+git://github.com/cloudify-cosmo/cloudify-amqp-influxdb.git@3.2m1",
+            "cloudify_dsl_parser": "git+git://github.com/cloudify-cosmo/cloudify-dsl-parser.git@3.2m1",
             "cloudify_manager": "-b 3.2m1 https://github.com/cloudify-cosmo/cloudify-manager.git",
         },
         "manager_rest_port": "8100",
